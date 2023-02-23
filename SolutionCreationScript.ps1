952ea28756219ee85a0403adc2724d4240348886
Guilder W. Milliner <guilderw@gmail.com>
2023-02-23 12:17:59 -0600

    Corected some configuration

--- SolutionCreationScript.ps1
+++ SolutionCreationScript.ps1
@@ -28,7 +28,7 @@ dotnet add reference ../$EntityName/$EntityName.csproj
 cd ..
 
 # create the BackEnd project
-dotnet new webapi -n $BackEndName -o $BackEndName --framework net6.0
+dotnet new webapi -n $BackEndName -o $BackEndName --no-https --framework net6.0
 cd $BackEndName
 dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection --version *
 dotnet add package Swashbuckle.AspNetCore --version 6.*
@@ -40,7 +40,7 @@ dotnet add reference ../$EntityName/$EntityName.csproj
 cd ..
 
 # create the FrontEnd project
-dotnet new mvc -n $FrontEndName -o $FrontEndName --framework net6.0
+dotnet new mvc -n $FrontEndName -o $FrontEndName --no-https --framework net6.0
 cd $FrontEndName
 dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 6.*
 dotnet add package Microsoft.AspNetCore.Mvc.NewtonsoftJson --version 6.*
@@ -49,9 +49,33 @@ dotnet add package Serilog.Sinks.Console --version *
 dotnet add package Serilog.Sinks.File --version *
 cd ..
 
+# remove the Class1.cs file from each project
+Remove-Item -Path "$basePath\$solutionName\$EntityName\Class1.cs" -Force
+Remove-Item -Path "$basePath\$solutionName\$DALName\Class1.cs" -Force
+
+#Remove WeatherForecast
+rm "$basePath\$solutionName\$BackEndName\Controllers\WeatherForecastController.cs"
+rm "$basePath\$solutionName\$BackEndName\WeatherForecast.cs"
+
+# add folders to each project
+mkdir MyFolder
+
 # create the solution file
 dotnet new sln -n $solutionName
-dotnet sln $solutionName.sln add ./$FrontEndName/$FrontEndName.csproj ./$EntityName/$EntityName.csproj ./$DALName/$DALName.csproj ./$BackEndName/$BackEndName.csproj
+dotnet sln $solutionName.sln add `
+    ./$EntityName/$EntityName.csproj `
+    ./$DALName/$DALName.csproj `
+    ./$BackEndName/$BackEndName.csproj `
+    ./$FrontEndName/$FrontEndName.csproj
+
+
+# Set starup Projects
+dotnet sln $solutionName.sln set startup-project $BackEndName/$BackEndName.csproj
+dotnet sln $solutionName.sln add $FrontEndName/$FrontEndName.csproj
+
+#Clean and build the solution
+dotnet clean
+dotnet build
 
 # open the solution
 Invoke-Item "$basePath\$solutionName\$solutionName.sln"
