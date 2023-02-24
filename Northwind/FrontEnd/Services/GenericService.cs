8a5a860fccbafc661c3e47dd96ca6bbaa58be941
Guilder W. Milliner <guilderw@gmail.com>
2023-02-24 17:23:40 -0600

    Tryed to implement a generic service, but that did not work

---
+++ Northwind/FrontEnd/Services/GenericService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+
+namespace FrontEnd.Services;
+
+public class GenericService<TModel> where TModel : class
+{
+    private readonly HttpService _httpService;
+    private readonly string _resourcePath;
+
+    public GenericService(IConfiguration configuration, string resourcePath)
+    {
+        _httpService = new HttpService(configuration);
+        _resourcePath = resourcePath;
+    }
+
+    public List<TModel> GetAll()
+    {
+        HttpResponseMessage responseMessage = _httpService.GetResponse(_resourcePath);
+        string content = responseMessage.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<List<TModel>>(content);
+    }
+
+    public TModel Get(int id)
+    {
+        HttpResponseMessage responseMessage = _httpService.GetResponse(_resourcePath + id);
+        string content = responseMessage.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<TModel>(content);
+    }
+
+    public TModel Create(TModel model)
+    {
+        HttpResponseMessage responseMessage = _httpService.PostResponse(_resourcePath, model);
+        string content = responseMessage.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<TModel>(content);
+    }
+
+    public TModel Edit(TModel model)
+    {
+        HttpResponseMessage responseMessage = _httpService.PutResponse(_resourcePath, model);
+        string content = responseMessage.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<TModel>(content);
+    }
+
+    public TModel Delete(int id)
+    {
+        HttpResponseMessage responseMessage = _httpService.DeleteResponse(_resourcePath + id);
+        string content = responseMessage.Content.ReadAsStringAsync().Result;
+        return JsonConvert.DeserializeObject<TModel>(content);
+    }
+}
