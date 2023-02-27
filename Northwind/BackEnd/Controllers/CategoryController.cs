9a2c4d80044a90a780ed85cf5e361a3ddac308dd
Guilder W. Milliner <guilderw@gmail.com>
2023-02-26 22:48:59 -0600

    Rename GenericControllers

--- Northwind/BackEnd/Controllers/CategoryController.cs
+++ Northwind/BackEnd/Controllers/CategoryController.cs
@@ -6,38 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BackEnd.Controllers;
 
-public class CategoryController : BaseApiController<Category, CategoryDto>
+public class CategoryController : GenericControllers<Category, CategoryDto>
 {
-	private readonly ILogger<CategoryController> _logger;
+    private readonly ILogger<CategoryController> _logger;
 
-	public CategoryController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CategoryController> logger) : base(
-		unitOfWork, mapper)
-	{
-		_logger = logger;
-	}
+    public CategoryController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CategoryController> logger) : base(
+        unitOfWork, mapper)
+    {
+        _logger = logger;
+    }
 
-	#region POST|Create - Used to create a new resource.
+    #region POST|Create - Used to create a new resource.
 
-	[HttpPost]
-	public override IActionResult Post([FromBody] CategoryDto requestDto)
-	{
-		_logger.LogInformation($"will look for Entity with of name {nameof(requestDto)} and see if we get it.");
+    [HttpPost]
+    public override IActionResult Post([FromBody] CategoryDto requestDto)
+    {
+        _logger.LogInformation($"will look for Entity with of name {nameof(requestDto)} and see if we get it.");
 
-		if (!ModelState.IsValid)
-		{
-			_logger.LogError($"Invalid POST attempt in {nameof(requestDto)}");
-			return BadRequest(ModelState);
-		}
+        if (!ModelState.IsValid)
+        {
+            _logger.LogError($"Invalid POST attempt in {nameof(requestDto)}");
+            return BadRequest(ModelState);
+        }
 
-		Category mappedResult = Mapper.Map<Category>(requestDto);
+        Category mappedResult = Mapper.Map<Category>(requestDto);
 
-		Repository.Insert(mappedResult);
-		UnitOfWork.SaveChanges();
+        Repository.Insert(mappedResult);
+        UnitOfWork.SaveChanges();
 
-		_logger.LogCritical($"The ID of Entity with of name {nameof(requestDto)} is {mappedResult.CategoryId} .");
+        _logger.LogCritical($"The ID of Entity with of name {nameof(requestDto)} is {mappedResult.CategoryId} .");
 
-		return CreatedAtAction(nameof(Get), new { id = mappedResult.CategoryId }, mappedResult);
-	}
+        return CreatedAtAction(nameof(Get), new { id = mappedResult.CategoryId }, mappedResult);
+    }
 
-	#endregion
+    #endregion
 }
--- Northwind/BackEnd/Controllers/GenericControllers.cs
+++ Northwind/BackEnd/Controllers/GenericControllers.cs
@@ -1,164 +1,126 @@
 ﻿using AutoMapper;
 using DAL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
 
 namespace BackEnd.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class GenericControllers<TEntity, TModel> : ControllerBase
-    where TEntity : class, new()
-    where TModel : class, new()
+public class BaseApiController<TEntity, TModel> : ControllerBase
+	where TEntity : class, new()
+	where TModel : class, new()
 {
-    private readonly ILogger<GenericControllers<TEntity, TModel>> _logger;
-    protected readonly IMapper Mapper;
-    protected readonly IGenericRepository<TEntity> Repository;
-    protected readonly IUnitOfWork UnitOfWork;
+	private readonly ILogger<BaseApiController<TEntity, TModel>> _logger;
+	protected readonly IMapper Mapper;
+	protected readonly IGenericRepository<TEntity> Repository;
+	protected readonly IUnitOfWork UnitOfWork;
 
-    public GenericControllers(IUnitOfWork unitOfWork, IMapper mapper)
-    {
-        try
-        {
-            UnitOfWork = unitOfWork;
-            Repository = UnitOfWork.GetRepository<TEntity>();
-            Mapper = mapper;
-            _logger = new LoggerFactory().CreateLogger<GenericControllers<TEntity, TModel>>();
-        }
-        catch (Exception ex)
-        {
-            Log.Error(ex, "An error occurred while initializing the GenericControllers.");
-            throw;
-        }
-    }
+	public BaseApiController(IUnitOfWork unitOfWork, IMapper mapper)
+	{
+		UnitOfWork = unitOfWork;
+		Repository = UnitOfWork.GetRepository<TEntity>();
+		Mapper = mapper;
+		_logger = new LoggerFactory().CreateLogger<BaseApiController<TEntity, TModel>>();
+	}
 
-    #region POST|Create - Used to create a new resource.
+	#region POST|Create - Used to create a new resource.
 
-    [HttpPost]
-    public virtual IActionResult Post([FromBody] TModel requestDto)
-    {
-        try
-        {
-            _logger.LogInformation($"Registration Attempt for {requestDto} ");
+	[HttpPost]
+	public virtual IActionResult Post([FromBody] TModel requestDto)
+	{
+		_logger.LogInformation($"Registration Attempt for {requestDto} ");
 
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning($"Invalid POST attempt in {nameof(requestDto)}");
-                return BadRequest(ModelState);
-            }
+		if (!ModelState.IsValid)
+		{
+			_logger.LogError($"Invalid POST attempt in {nameof(requestDto)}");
+			return BadRequest(ModelState);
+		}
 
-            TEntity? mappedResult = Mapper.Map<TEntity>(requestDto);
+		TEntity? mappedResult = Mapper.Map<TEntity>(requestDto);
 
-            Repository.Insert(mappedResult);
-            UnitOfWork.SaveChanges();
+		Repository.Insert(mappedResult);
+		UnitOfWork.SaveChanges();
 
-            return Ok(mappedResult);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error occurred while creating a new resource.");
-            throw;
-        }
-    }
+		return Ok(mappedResult);
+	}
 
-    #endregion
+	#endregion
 
-    #region PUT|Update - Used to update an existing resource.
+	#region PUT|Update - Used to update an existing resource.
 
-    [HttpPut]
-    public IActionResult Put([FromBody] TModel requestDto)
-    {
-        try
-        {
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning($"Invalid PUT attempt in {nameof(requestDto)}");
-                return BadRequest(ModelState);
-            }
+	[HttpPut]
+	public IActionResult Put([FromBody] TModel requestDto)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
 
-            TEntity? mappedResult = Mapper.Map<TEntity>(requestDto);
-            Repository.Update(mappedResult);
-            UnitOfWork.SaveChanges();
+		TEntity? mappedResult = Mapper.Map<TEntity>(requestDto);
+		Repository.Update(mappedResult);
+		UnitOfWork.SaveChanges();
 
-            return Ok(mappedResult);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error occurred while updating an existing resource.");
-            throw;
-        }
-    }
+		return Ok(mappedResult);
+	}
 
-    #endregion
+	#endregion
 
-    #region PATCH|Update - Used to partially update an existing resource.
+	#region PATCH|Update - Used to partially update an existing resource.
 
-    [HttpPatch]
-    public IActionResult Patch()
-    {
-        try
-        {
-            throw new NotImplementedException();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error occurred while partially updating an existing resource.");
-            throw;
-        }
-    }
+	[HttpPatch]
+	public IActionResult Patch()
+	{
+		throw new NotImplementedException();
+	}
 
-    #endregion
+	#endregion
 
-    #region DELETE|Delete - Used to delete a resource.
+	#region DELETE|Delete - Used to delete a resource.
 
-    [HttpDelete("{id:int}")]
-    public IActionResult Delete(int id)
-    {
-        try
-        {
-            if (!ModelState.IsValid)
-            {
-                _logger.LogWarning($"Invalid DELETE attempt with {nameof(id)} {id}");
-                return BadRequest(ModelState);
-            }
+	[HttpDelete("{id:int}")]
+	public IActionResult Delete(int id)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
 
-            TEntity dbResult = Repository.Get(id);
-            Repository.Remove(dbResult);
-            UnitOfWork.SaveChanges();
+		TEntity dbResult = Repository.Get(id);
+		Repository.Remove(dbResult);
+		UnitOfWork.SaveChanges();
 
-            return NoContent();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error occurred while deleting a resource.");
-            throw;
-        }
-    }
+		return NoContent();
+	}
 
-    #endregion
+	#endregion
 
-    #region GET|Read - Used to retrieve a resource or a collection of resources.
+	#region GET|Read - Used to retrieve a resource or a collection of resources.
 
-    [HttpGet]
-    public IActionResult Get()
-    {
-        try
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+	[HttpGet]
+	public IActionResult Get()
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
 
-            IEnumerable<TEntity> dbResult = Repository.GetAll();
-            IList<TModel>? mappedResult = Mapper.Map<IList<TModel>>(dbResult);
-            return Ok(mappedResult);
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "An error occurred while retrieving resources.");
-            return StatusCode(500);
-        }
-    }
+		IEnumerable<TEntity> dbResult = Repository.GetAll();
+		IList<TModel>? mappedResult = Mapper.Map<IList<TModel>>(dbResult);
+		return Ok(mappedResult);
+	}
 
-    #endregion
+	[HttpGet("{id:int}")]
+	public IActionResult Get(int id)
+	{
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(ModelState);
+		}
+
+		TEntity dbResult = Repository.Get(id);
+		TModel? mappedResult = Mapper.Map<TModel>(dbResult);
+		return Ok(mappedResult);
+	}
+
+	#endregion
 }
